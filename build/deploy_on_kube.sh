330bdec5ae8a1d3e3b0b0efb3cc95183845eb617
JayM <jay.mojnidar@gmail.com>
2020-02-07 00:25:07 -0800

    green_deployment checkin

--- build/deploy_on_kube.sh
+++ build/deploy_on_kube.sh
@@ -5,7 +5,7 @@ TARGET_FILE=/Users/jaymojnidar/work/code/arun/github.com/uda_devops_capstone/dep
 SERVICE_FILE=/Users/jaymojnidar/work/code/arun/github.com/uda_devops_capstone/infra/capstone_service.json
 MESSAGE=`cat /Users/jaymojnidar/work/code/arun/github.com/uda_devops_capstone/infra/rc_template.json`
 echo $MESSAGE
-NEWM=${MESSAGE/BUILD_NUMBER/$1:$2}
+NEWM=${MESSAGE/BUILD_NUMBER/$1:g$2}
 #NEWM=${MESSAGE/passion/desire}
 echo $NEWM > $TARGET_FILE
 cat $TARGET_FILE
